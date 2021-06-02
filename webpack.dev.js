5f86490c5dab5332a8d664e5831958f380847188
Jimmy Chuk <jimmychuk@outlook.com>
2021-06-01 20:35:44 -0400

    last update

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,6 +19,8 @@ module.exports = {
         ]
     },
     plugins: [
+        new BundleAnalyzerPlugin(),
+
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
